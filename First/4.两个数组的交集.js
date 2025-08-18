4eedd10716da0d6d17f0c72e4df05511a318dfd4
View <lijunpg@foxmail.com>
2025-08-19 00:02:32 +0800

    test

--- First/4.两个数组的交集.js
+++ First/4.两个数组的交集.js
@@ -37,3 +37,5 @@ var intersection = function (nums1, nums2) {
   return res;
 };
 
+console.log(intersection((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])));
+console.log(intersection((nums1 = [4, 9, 5]), (nums2 = [9, 4, 9, 8, 4])));
