b5427688088662eb589a58d7f3c56f4e5c62043f
View <lijunpg@foxmail.com>
2025-08-20 23:15:01 +0800

    feat:移动零

--- First/6. 移动零.js
+++ First/6. 移动零.js
@@ -2,6 +2,32 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// 1.暴力解法
+// var moveZeroes = function (nums) {
+//   let count = 0;
+//   for (let i = nums.length; i >= 0; i--) {
+//     if (nums[i] == 0) {
+//       nums.splice(i, 1);
+//       count++;
+//     }
+//   }
+
+//   for (let i = 0; i < count; i++) {
+//     nums.push(0);
+//   }
+//   return nums;
+// };
+
+// console.log(moveZeroes((nums = [0, 1, 0, 3, 12])));
+// console.log(moveZeroes((nums = [0])));
+// console.log(moveZeroes((nums = [0, 0, 1])));
+
+
+// 2.双指针法——通过计算0的个数来移动数组
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 var moveZeroes = function (nums) {
   let count = 0; //计算数组中0的个数
 
