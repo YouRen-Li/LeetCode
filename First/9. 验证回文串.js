031f52f988db68afaad3fcfe51a0ee93e470efed
View <lijunpg@foxmail.com>
2025-08-25 23:16:36 +0800

    feat:验证回文串

--- First/9. 验证回文串.js
+++ First/9. 验证回文串.js
@@ -3,10 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  let char = "";
-  s = s.toLowerCase();
+  let char = ""; //定义一个空字符串保存新的字符
+  s = s.toLowerCase(); //把字符串转换为小写
   for (let i = 0; i < s.length; i++) {
+    //遍历字符串
     if (
+      //如果字符串的unicode编码在48-57，97-122之间，说明就说在0-9，a-z之间
+      // 那么就保存到空的字符串里
       (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122) ||
       (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57)
     ) {
@@ -14,6 +17,10 @@ var isPalindrome = function (s) {
     }
   }
 
+  // 这里用了3个方法，split把字符串通过""剪切为数组，
+  // reverse反转数组，join把数组拼成字符串
+  // 把原来的新数组和反转后拼接的进行比较，如果一致就是true
+  // 不一致就是false
   return char == char.split("").reverse().join("");
 };
 
