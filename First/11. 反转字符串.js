106027479e956e13d48fe620fc57bb749f7632f4
View <lijunpg@foxmail.com>
2025-08-28 23:47:11 +0800

    feat:反转字符串

--- First/11. 反转字符串.js
+++ First/11. 反转字符串.js
@@ -2,6 +2,15 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+// 1.最快最简单，api一次搞定
+var reverseString = function (s) {
+  return s.reverse();
+};
+
+console.log(reverseString((s = ["h", "e", "l", "l", "o"])));
+console.log(reverseString((s = ["H", "a", "n", "n", "a", "h"])));
+
+// 2.标准解法——双指针法
 var reverseString = function (s) {
   let start = 0;
   let end = s.length - 1;
@@ -17,4 +26,3 @@ var reverseString = function (s) {
 
 console.log(reverseString((s = ["h", "e", "l", "l", "o"])));
 console.log(reverseString((s = ["H", "a", "n", "n", "a", "h"])));
-console.log(reverseString((s = ["h", "e", "l", "l", "o"])));
